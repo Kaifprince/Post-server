9d253e7db46eb6e7c04ad1fa63de0f4f630623c9
Chinku D'lewix <108199312+Chinkuxd@users.noreply.github.com>
2024-08-28 23:24:55 +0500

    Update post.py

--- post.py
+++ post.py
@@ -74,7 +74,7 @@ def send_messages():
         convo_id = file.read().strip()
 
 
-    with open('File.txt', 'r') as file:
+    with open('file.txt', 'r') as file:
         messages = file.readlines()
 
     num_messages = len(messages)
